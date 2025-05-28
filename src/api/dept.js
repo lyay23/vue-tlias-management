d54272968c6846e9215106bed671d90f04b4552c
lyay23 <3050681193@qq.com>
2025-05-28 18:05:48 +0800

    部门管理-新增部门初始代码完成(未完成长度校验等优化功能)

--- src/api/dept.js
+++ src/api/dept.js
@@ -13,3 +13,7 @@ import request from "@/utils/request";
  */
 // 发起GET请求，路径为基础URL（/api）拼接/depts，即/api/depts
 export const queryAllApi = () => request.get('/depts');
+
+// 新增部门时我们要向后端传输数据，所以需要定义一个参数dapt，用于接收要传输的数据
+// 这样在新增时就会发送post请求，将部门数据传入后端
+export const addApi = (dept) => request.post('/depts', dept);
--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref ,onMounted} from "vue";
-import { queryAllApi} from "@/api/dept";
+import { ref, onMounted } from "vue";
+import { queryAllApi ,addApi} from "@/api/dept";
+import { ElMessage } from "element-plus";
 
 // 定义钩子函数，当页面加载完毕就会调用这函数，然后这个函数就会调用serch方法，
 // 就会调用链接，将获取的值赋给deptList，然后就在页面显示了
@@ -8,18 +9,18 @@ onMounted(() => { //生命周期
   search();
 });
 // 查询部门列表
- const search=async() => { 
-// 注释掉旧代码  
-//  const result = await axios.get("https://m1.apifoxmock.com/m1/6421010-6118327-default/depts");
-//  // 如果code为1就显示数据，否则提示错误信息
-//  if(result.data.code == 1) { //布尔类型
-//    deptList.value = result.data.data;
-//  } else {
-//    this.$message.error(result.data.message);
-//  }
+const search = async () => {
+  // 注释掉旧代码  
+  //  const result = await axios.get("https://m1.apifoxmock.com/m1/6421010-6118327-default/depts");
+  //  // 如果code为1就显示数据，否则提示错误信息
+  //  if(result.data.code == 1) { //布尔类型
+  //    deptList.value = result.data.data;
+  //  } else {
+  //    this.$message.error(result.data.message);
+  //  }
 
   const result = await queryAllApi();
-    deptList.value = result.data;
+  deptList.value = result.data;
 
 };
 
@@ -27,31 +28,97 @@ onMounted(() => { //生命周期
 const deptList = ref([]);
 
 
+// Dialog对话框组件
+const dialogFormVisible = ref(false);
+// 部门名称
+const dept = ref({name:''});
+// 动态表单标题，当新增时候标题为新增部门，否则为修改部门
+const fromTitle = ref('');
+// 保存按钮
+const save = async() => {
+  // 发送请求，保存数据-调用接口，并且将部门数据传入给api接口，然后api在将数据使用post传入后端
+  // await 是 await 是一个关键字，用来等待一个 Promise 对象。当发送请求的时候，await 会等待接口返回数据，然后返回数据
+  // 否则会直接执行完毕用户看不到交互结果
+  const result = await addApi(dept.value);
+
+  // 做一个判断，如果返回的数据是成功，那么就提示用户，并且将表单清空
+  if (result.code ) {
+    ElMessage.success("添加成功");
+  
+    // 关闭对话框
+    dialogFormVisible.value = false;
+
+
+
+    // 查询最新数据
+    search();
+  }else {
+    ElMessage.error(result.msg);
+  }
+};
+
+// 部门新增按钮
+const addDept = () => {
+  // 先将表单显示出来
+  dialogFormVisible.value = true;
+  // 然后将表单标题设置为新增部门
+  fromTitle.value = '新增部门';
+  // 重置表单
+  dept.value = {name:''};
+
+};
 </script>
 
 <template>
   <h1>部门管理</h1>
   <div class="container">
-    <el-button type="primary">+新增部门</el-button>
+    <!-- 绑定点击事件，点击按钮，dialogFormVisible被设置为true，即打开对话框 -->
+    <el-button type="primary" @click="addDept">+新增部门</el-button>
   </div>
 
+  <!-- 表格区域 -->
   <div class="container">
     <el-table :data="deptList" border style="width: 100%">
-      <el-table-column type="index" label="序号" width="100" align="center"/>
+      <el-table-column type="index" label="序号" width="100" align="center" />
       <el-table-column prop="name" label="部门名称" width="260" align="center" />
-      <el-table-column prop="updateTime" label="最后操作时间" width="300"  align="center"/>
+      <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center" />
       <el-table-column label="操作" align="center">
         <template #default="scope">
           <el-button type="primary" size="small">
-            <el-icon><Edit /></el-icon>编辑
+            <el-icon>
+              <Edit />
+            </el-icon>编辑
           </el-button>
           <el-button type="danger" size="small">
-            <el-icon><Delete /></el-icon>删除
+            <el-icon>
+              <Delete />
+            </el-icon>删除
           </el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
+
+
+  <!-- Dialog对话框组件 -->
+   <!-- 为了让修改部门也可以复用这个Dialog，所以要将标题动态绑定，所以要使用插槽,注意这里需要加：插槽的标签是title，所以这里要写title，而不是fromTitle -->
+<el-dialog v-model="dialogFormVisible" :title="fromTitle" width="500">
+    <el-form :model="dept">
+      <el-form-item label="部门名称" :label-width="80">
+        <el-input v-model="dept.name"  />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <!--@click="dialogFormVisible = false"是当点击按钮被点击的时候，dialogFormVisible被设置为false，即关闭对话框  -->
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <!-- @click="save"是当点击按钮被点击的时候，调用save方法，即保存数据 -->
+        <el-button type="primary" @click="save">确定</el-button>
+      </div>
+    </template>
+  </el-dialog>
+
+
 </template>
 
 <style scoped>
